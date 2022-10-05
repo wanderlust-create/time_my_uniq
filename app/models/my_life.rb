fc4a89c0fec0f5523e9ca2232b91a9baa35d0725
Tamara Dowis <tamara.dowis@gmail.com>
2022-10-05 11:47:30 -0500

    life_parts model with rspec

--- app/models/my_life.rb
+++ app/models/my_life.rb
@@ -1,7 +1,7 @@
 class MyLife < ApplicationRecord
   belongs_to :user
 
-  # has_many: life_parts
+  has_many :life_parts
 	# has_many: goals, through life_parts
 	# has_many: actions, through goals
 	# has_many: tasks, through actions
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_155823) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_163641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "life_parts", force: :cascade do |t|
+    t.bigint "my_life_id", null: false
+    t.string "name"
+    t.text "description"
+    t.float "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["my_life_id"], name: "index_life_parts_on_my_life_id"
+  end
 
   create_table "my_lives", force: :cascade do |t|
     t.text "description"
@@ -30,5 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_155823) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "life_parts", "my_lives"
   add_foreign_key "my_lives", "users"
 end
