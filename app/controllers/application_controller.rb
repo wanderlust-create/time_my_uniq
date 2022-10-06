fe0eb9a3f2f61851c1a8d27387aaeb64ef7c3b23
Tamara Dowis <tamara.dowis@gmail.com>
2022-10-06 16:50:37 -0500

    add login methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+    def current_user
+        if session[:access_token]
+            session[:access_token]
+        end
+    end
+
+    def require_user
+        if!current_user
+            flash[:alert] = "Please log in or register for an account"
+            redirect_to root_path
+        end 
+    end
+
+    def logged_in?
+        !!current_user
+    end 
+
 end
