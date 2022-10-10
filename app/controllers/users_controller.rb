73b17d1bdfc75bbc86420ac8d4e9539a27b5fa44
Tamara Dowis <tamara.dowis@gmail.com>
2022-10-10 13:04:36 -0500

    update users controller create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,29 @@
 class UsersController < ApplicationController
-    before_action :require_user, except [:authorize]
-    def authorize
-        auth_hash = request.env['omniauth.auth']
-        session[:access_token] = auth_hash[:credentials][:token]
-        session[:email] = auth_hash[:info][:email]
-        redirect_to dashboard_path
-    end 
+  before_action :require_user, only: :dashboard
 
-    def index
-        
-    end
-
-
+  def create
+    auth_hash = request.env['omniauth.auth']
+    email = auth_hash['info']['email']
+    name = auth_hash['info']['name']
+    user = User.find_by(email: email)
+    if user
+      session[:access_token] = auth_hash['credentials']['token']
+      session[:user_id] = user.id
+      redirect_to '/dashboard'
+    else
+      new_user = User.create!(email: email, user_name: name)
+      session[:access_token] = auth_hash['credentials']['token']
+      session[:user_id] = new_user.id
+      redirect_to '/dashboard'
+  end
 end 
+
+  def logout
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+  def dashboard
+    @user = User.find(session[:user_id])
+  end
+end
